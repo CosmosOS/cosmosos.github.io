69abadc9d2fe36034b303492adec433acb607b18
valentinbreiz <valentinbreiz@users.noreply.github.com>
2026-08-22 19:47:14 +0000

    deploy: CosmosOS/Cosmos@b35ec7e5c3b3b7f4b3f4561fc25a4005a85f01b6

--- public/main.js
+++ public/main.js
@@ -1,1 +1,29 @@
-export default {}
+// Swap the diagram SVGs with their baked dark variants when the site theme is
+// dark. The SVGs also carry a prefers-color-scheme media query for renderers
+// we don't control (github.com), but browsers that pin the media query to
+// light (LibreWolf's resistFingerprinting, older engines) would otherwise
+// always show the light variant; keying on data-bs-theme follows the site
+// toggle everywhere.
+const diagramSrc = /^(.*\/images\/diagrams\/gc-[a-z-]+?)(-dark)?\.svg$/;
+
+function applyDiagramTheme() {
+    const dark = document.documentElement.getAttribute('data-bs-theme') === 'dark';
+    for (const img of document.querySelectorAll('img[src*="images/diagrams/"]')) {
+        const m = img.src.match(diagramSrc);
+        if (!m) {
+            continue;
+        }
+        const src = m[1] + (dark ? '-dark' : '') + '.svg';
+        if (img.src !== src) {
+            img.src = src;
+        }
+    }
+}
+
+export default {
+    start: () => {
+        new MutationObserver(applyDiagramTheme)
+            .observe(document.documentElement, { attributes: true, attributeFilter: ['data-bs-theme'] });
+        applyDiagramTheme();
+    }
+}
